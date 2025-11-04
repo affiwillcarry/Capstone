2b00957b71ef587b1c63b0c5f09e86e09c4db0b3
affiwillcarry <mohammedarafat663@gmail.com>
2025-11-03 18:39:49 -0700

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
 document.getElementById("book-now").addEventListener("click", () => {
   alert("Feature coming soon! (Appointment booking page in next update)");
   window.location.href = "/book";
-});
+}); 
